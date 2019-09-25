743bdef1456c6b2ac6a080ebf04c9ac955f4801e
dunji <38369025+dunji@users.noreply.github.com>
2019-09-25 12:07:08 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,6 +2,7 @@
 
 yum install -y expect 
 wget -N --no-check-certificate https://raw.githubusercontent.com/dunji/test/master/ssr.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/dunji/test/master/auto.sh
 chmod +x ssr.sh
 chmod +x auto.sh
 ./auto.sh

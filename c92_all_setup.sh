4f541a4f4c5db3a7e318a2668dc06a4dae251954
dunji <38369025+dunji@users.noreply.github.com>
2023-10-15 11:16:36 +0800

    Add files via upload

--- c92_all_setup.sh
+++ c92_all_setup.sh
@@ -9,16 +9,22 @@ systemctl disable firewalld.service
 systemctl start crond.service
 systemctl enable crond.service
 chmod +x /etc/rc.d/rc.local
-echo -e "\n" >> /etc/rc.local
-echo -e "rm -rf /var/log/shadowsocksr.log\n" >> /etc/rc.local
-echo -e "sleep 15\n" >> /etc/rc.local
-echo -e "screen -dmS ssr bash -c '/root/op.sh'" >> /etc/rc.local
-echo -e "10 3 * * * /root/c92_restart.sh" > /var/spool/cron/root
+echo -e "\n"                                          >> /etc/rc.local
+echo -e "rm -rf /var/log/shadowsocksr.log"            >> /etc/rc.local
+echo -e "sleep 15"                                    >> /etc/rc.local
+echo -e "screen -dmS ssr bash -c '/root/op.sh'"       >> /etc/rc.local
+echo -e "while true"                                  >> /etc/rc.local
+echo -e "do"                                          >> /etc/rc.local
+echo -e "sleep 60"                                    >> /etc/rc.local
+echo -e "if test -z "$(screen -ls | grep ssr)"; then" >> /etc/rc.local
+echo -e "screen -dmS ssr bash -c '/root/op.sh'"       >> /etc/rc.local
+echo -e "fi"                                          >> /etc/rc.local
+echo -e "done"                                        >> /etc/rc.local
+echo -e "10 3 * * * screen -S ssr -X quit"            >  /var/spool/cron/root
+echo -e "11 3 * * * rm -rf /var/log/shadowsocksr.log" >> /var/spool/cron/root
 
 wget --no-check-certificate https://raw.githubusercontent.com/dunji/test/master/op.sh
-wget --no-check-certificate https://raw.githubusercontent.com/dunji/test/master/c92_restart.sh
 chmod +x op.sh
-chmod +x c92_restart.sh
 
 cd
 mkdir ssr
